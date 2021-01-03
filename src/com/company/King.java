a0c71effb812970c50246bb2519eb9507d5ab221
Barry <bbalasingham@gmail.com>
2021-01-02 19:25:55 -0700

    [King] Working progress

--- src/com/company/King.java
+++ src/com/company/King.java
@@ -10,9 +10,34 @@ public class King extends Piece {
         /**
          * mult will tell me if the player is white or black
          * isKilling tells me if the player is taking an enemy piece
+         * isCastling decides if the King is castling
          */
         int mult = Player.PlayerType.White == this.type ? 1 : -1;
         boolean isKilling = false;
+
+        if (mult == 1 && beg.r == 3 && beg.c == end.c && end.r == 1 && board.grid[1][0] == null/* King is trying to castle */ && board.grid[0][0] != null/* Rook is in correct position */) {
+            // King is White
+            if (board.grid[2][0] == null) {
+                // Check if knight and bishop spaces are free
+                board.grid[2][0] = board.grid[0][0];
+                board.grid[1][0] = board.grid[3][0];
+                board.grid[0][0] = null;
+                board.grid[3][0] = null;
+                return true;
+
+            }
+        } else if (mult == -1 && beg.r == 4 && end.r == 6 && beg.c == end.c && board.grid[6][7] == null/* King is trying to castle */ && board.grid[7][7] != null/* Rook is in correct position */) {
+            // King is Black
+            if (board.grid[5][7] == null) {
+                // Check if knight and bishop spaces are free
+                board.grid[6][7] = board.grid[4][7];
+                board.grid[5][7] = board.grid[7][7];
+                board.grid[4][7] = null;
+                board.grid[7][7] = null;
+                return true;
+
+            }
+        }
 
         if (board.grid[end.r][end.c] != null) {
             isKilling = true;
